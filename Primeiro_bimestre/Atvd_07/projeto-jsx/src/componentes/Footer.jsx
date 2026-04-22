b5715a892f3e66d11db94cc6ce3169eca14a7f02
fagschneider <fagschneider@minha.fag.edu.br>
2026-04-22 13:53:19 -0300

    Revert "Atvd_07"
    This reverts commit 2764ac14e582873cd24f5f6b6824415289350572.

--- Primeiro_bimestre/Atvd_07/projeto-jsx/src/componentes/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-    <footer>
-      <p>&copy; 2024 - Todos os direitos reservados.</p>
-    </footer>
-    )
-}
-
-export default Footer
--- Primeiro_bimestre/Atvd_07/projeto-jsx/src/componentes/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(){
-    return(
-        <header>
-            <h1>Meu Blog de Viagens</h1>
-        </header>
-    )
-}
-
-export default Header
